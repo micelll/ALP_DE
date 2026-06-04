b77bbb05ec3c905387142ecc5d63b58e4a8f78c0
Amanda <amandarenatago05@gmail.com>
2026-06-04 10:10:11 +0700

    revisi fact_sales.sql

--- dbt/online_retail/models/marts/fact_sales.sql
+++ dbt/online_retail/models/marts/fact_sales.sql
@@ -15,9 +15,9 @@ dim_date as (
 )
 
 select
+    -- Kunci di-generate setelah datanya dipastikan unik per invoice & item
     md5(t.invoice_id || t.stock_code || t.invoice_date::text) as fact_key,
     
-    -- foreign keys
     c.customer_key,
     p.product_key,
     d.date_key,
@@ -25,13 +25,18 @@ select
     t.invoice_id,
     t.country,
     t.is_return,
+    t.invoice_date,
     
-    t.quantity,
-    t.unit_price,
-    t.line_total,
-    t.invoice_date
+    -- Lakukan agregasi untuk mengeliminasi duplikat item di invoice yang sama
+    sum(t.quantity) as quantity,
+    max(t.unit_price) as unit_price, -- ambil harga tertinggi atau rata-rata jika variatif
+    sum(t.line_total) as line_total
 
 from transactions t
 left join dim_customers c on t.customer_id = c.customer_id
 left join dim_products p  on t.stock_code  = p.stock_code
 left join dim_date d      on t.invoice_date_day = d.date_day
+group by 
+    t.invoice_id, t.stock_code, t.invoice_date, 
+    c.customer_key, p.product_key, d.date_key, 
+    t.country, t.is_return
